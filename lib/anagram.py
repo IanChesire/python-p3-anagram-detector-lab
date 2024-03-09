a0676a5216b18f980fb0770d844c068df22af325
Ian <iankosgey535@gmail.com>
2024-03-09 11:48:49 +0300

    Checks if word is an anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,22 @@
-# your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        original_word = self.word.replace(" ", "")
+        matched_words = []
+
+        for word in word_list:
+            word_to_match = word.replace(" ", "")
+            
+            if word_to_match == original_word or len(word_to_match) != len(original_word):
+                continue
+
+            for char in word_to_match:
+                if char not in original_word:
+                    break
+            else:
+                matched_words.append(word)
+
+        return matched_words
